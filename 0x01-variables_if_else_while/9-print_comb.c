23bdd7d386c5974d76c022eee71fda1379655fe5
klassiqk <klassiqkerrish@gmail.com>
2023-03-18 13:55:55 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,16 @@ int main(void)
 
 	for (i = o ; i < 10 ; i++)
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+
+		       putchar(i + '0');
+		       putchar(',');
+		       putchar(' ');
+		}
 	}
-	putchar('\n');
+
 	return (0);
 }
