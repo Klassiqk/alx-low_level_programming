0ffbe2396fa1ebb1dae4d1ae9b03a100991a5f41
klassiqk <klassiqkerrish@gmail.com>
2023-03-22 20:14:48 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * jack bauer - Prints every minute of the day of jack bauer
+ * jack_bauer - Prints every minute of the day of jack bauer
  * Return: ...
  */
 
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
@@ -26,3 +26,8 @@ void jack_bauer(void)
 	_putchar(d + '0');
 	_putchar('\n');
 	}
+	}
+	}
+	}
+	}
+}
