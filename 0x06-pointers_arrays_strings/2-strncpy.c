1fa17dfac4b6b9af14f0d3ef409cf522562dc5c8
klassiqk <klassiqkerrish@gmail.com>
2023-03-29 15:18:27 -0700

    mean

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
- * *_strncpy -function that copies string
- * @dest: destination string pointer
- * @src: source string pointer
- * @n: number of bytes to be used
- * Return: pointer to destination of string
+ * *_strncpy -  copies a string
+ * @dest: input values
+ * @src: input value
+ * @n: input value
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int j;
 
-for (i = 0 ; i < n ; i++)
-dest[i] = src[i];
-return (dest);
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[j] = src[j];
+	j++;
+	}
+	while (j < n)
+	{
+	dest[j] = '\0';
+	j++;
+	}
+	return (dest);
 }
