502639ec62392d6be0b738fd8ee77b499e9761a0
klassiqk <klassiqkerrish@gmail.com>
2023-03-29 20:49:11 -0700

    all good

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to be capitalized
- * Return: a pointer to the changed string
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
-
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-int index = 0;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-while (str[index])
-{
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
-
-if (str[index - 1] = ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-src[index - 1] == ',' ||
-src[index - 1] == ';' ||
-src[index - 1] == '.' ||
-src[index - 1] == '!' ||
-src[index - 1] == '?' ||
-src[index - 1] == '"' ||
-src[index - 1] == '(' ||
-src[index - 1] == ')' ||
-src[index - 1] == '{' ||
-src[index - 1] == '}' ||
-index == 0)
-	str[index] -= 32;
-
-index++;
-}
-return (str);
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
 }
