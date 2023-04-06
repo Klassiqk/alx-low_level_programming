93f37818563798b5b32b4a5d19fa60473217ec06
klassiqk <Klassiqk>
2023-04-06 16:04:50 -0700

    recurr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - return length of string
- * @s: string parameter
+ * _strlen_recursion - return length of stringg
+ * @s: string to be measured
  * Reurn: length of string
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-	return (0);
-	s++;
-	return (1 + _strlen_recursion(s));
+	int longit = 0;
+
+	if (*s)
+	{
+	longit++;
+	longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
 }
