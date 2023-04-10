7514a7179d7155df159b3e096a240ee25efebba8
klassiqk <Klassiqk>
2023-04-09 23:44:29 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - function to print it's name
+ * @argc: argc parameter
+ * @argv: array of command listed
+ * Return: 0 for success
+ */
+
+int main(int argc. char *argv[])
+{
+	printf("%s\n", *argv);
+}
