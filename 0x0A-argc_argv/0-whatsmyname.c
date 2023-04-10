a6f140d601b1affd8994b13ec72a102979929629
klassiqk <Klassiqk>
2023-04-10 01:02:45 -0700

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -6,7 +7,7 @@
  * Return: 0 for success
  */
 
-int main(int argc char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
